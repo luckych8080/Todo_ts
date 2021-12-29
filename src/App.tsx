7a2bb98b9b1218765de2fd9328a8698d58413310
Lucky <luckych8080@gmail.com>
2021-12-29 16:41:07 +0530

    set up input value

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    //checking element by name 
+    if(event.target.name === "task"){
+      setTask(event.target.value)
+    }else{
+      setDeadline(Number(event.target.value))
+    }
+  };
+
+  
+
   return (
-    <h1 className='App'>Hello lucky you are fucked</h1>
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            name="task"
+            placeholder="Task..."
+            onChange={changeHandler}
+          />
+          <input
+            type="number"
+            name="deadline"
+            placeholder="DeadLine (in Days)..."
+            onChange={changeHandler}
+          />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
   );
-}
+};
 
 export default App;

d10bd3c25395b46ae3698043c44c77f3f9c0163a
Lucky <luckych8080@gmail.com>
2021-12-29 17:37:14 +0530

    complete basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import React, { ChangeEvent, FC, useState } from "react";
 import "./App.css";
+import { ITask } from "./Interfaces";
+import TodoTask from "./TodoTask";
 
 const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadLine, setDeadline] = useState<number>(0);
-  const [todo, setTodo] = useState([]);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
 
-  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    //checking element by name 
-    if(event.target.name === "task"){
-      setTask(event.target.value)
-    }else{
-      setDeadline(Number(event.target.value))
+  const ChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    //checking element by name & assigning value
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
     }
   };
 
-  
+  const AddTodoHandler = (): void => {
+    // adding tasks
+    const newTask = { taskName: task, deadLine: deadLine };
+    setTodoList([...todoList, newTask]);
+    // can add pervious medthod on setTodoList that is useful
+    setTask("");
+    setDeadline(0);
+  };
+
+  const CompleteTaskHandler = (taskNameToDelete: string): void => {
+    // delete a task
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete
+    }))
+  }
+
 
   return (
     <div className="App">
@@ -25,18 +42,24 @@ const App: FC = () => {
             type="text"
             name="task"
             placeholder="Task..."
-            onChange={changeHandler}
+            value={task}
+            onChange={ChangeHandler}
           />
           <input
             type="number"
             name="deadline"
             placeholder="DeadLine (in Days)..."
-            onChange={changeHandler}
+            value={deadLine}
+            onChange={ChangeHandler}
           />
         </div>
-        <button>Add Task</button>
+        <button onClick={AddTodoHandler}>Add Task</button>
       </div>
-      <div className="todoList"></div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={CompleteTaskHandler} />;
+        })}
+      </div>
     </div>
   );
 };
